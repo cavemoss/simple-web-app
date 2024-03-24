c0e7217a00bcbc7996bd4d6005ee344a11df8b17
mossy boy <alexshu1812@gmail.com>
2024-03-24 12:54:47 +0800

    3

--- client/src/Base.jsx
+++ client/src/Base.jsx
@@ -1,4 +1,4 @@
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 import { useNavigate, Outlet } from 'react-router-dom'
 
 function Base(){
@@ -8,13 +8,37 @@ function Base(){
     useEffect(() => {
         navigate('/home')
     }, [])
+
+    const [display, setDisplay] = useState(false)
+
+    const root = document.querySelector(':root')
+
+    root.style.setProperty('--content-width', window.innerWidth + 'px')
+    if(window.innerWidth > 600) {
+        root.style.setProperty('--navbar-left', '0')
+        root.style.setProperty('--content-margin', '260px')    
+    } else {
+        if(display){
+            root.style.setProperty('--navbar-left', '0')
+            root.style.setProperty('--content-margin', '260px')
+        } else {
+            root.style.setProperty('--navbar-left', '-200px')
+            root.style.setProperty('--content-margin', '60px')
+        }  
+    }
+
+    function slide(){
+        display ? setDisplay(false) : setDisplay(true)
+    }
     
     return(
         <>
-            <div className="navbar">
-                <p onClick={() => navigate('/home')}>Home</p>
-                <p onClick={() => navigate('/create')}>Create</p>
-                <p onClick={() => navigate('/lists')}>Lists</p>
+            <div onClick={slide} className="navbar">
+                <div className="options">
+                    <p onClick={() => navigate('/home')}>Home</p>
+                    <p onClick={() => navigate('/create')}>Create</p>
+                    <p onClick={() => navigate('/lists')}>Lists</p>
+                </div>    
             </div>
             <div className="content">
                 <Outlet />
